6806fdec5f6d2056a3c5654465c001936a4efa7e
donchris111 <143852269+donchris111@users.noreply.github.com>
2025-02-28 14:36:53 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
